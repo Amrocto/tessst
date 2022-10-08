bc7dcaf2f6cf8e4c0c5198ce71186f9283576e6e
Amrocto <97044808+Amrocto@users.noreply.github.com>
2022-10-08 20:20:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+AOS.init();
+console.log('hello')
+const telega = document.querySelector('#telega')
+const telega2 = document.querySelector('#telega2')
+console.log(telega)
+// telega.addEventListener("mouseenter", function(index){
+//     telega.classList.toggle("hidden")
+//     telega2.classList.toggle("hidden")
+// })
+// telega2.addEventListener("mouseenter",function(index){
+    
+// })
